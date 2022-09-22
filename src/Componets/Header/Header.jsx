3a0f3cee6b34a2ae1a6a54fd7cf18c2499b535e2
Egor Pikalov <pikaov.ea97@gmail.com>
2022-09-23 02:20:54 +0300

    init commit "Добавлен лого в header"

--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -3,12 +3,11 @@ import "./header.scss"
 function Header(){
       return(
             <div className="Header">
-                  <h2>REACT PIZZA </h2>
-                  {/*<img className="search_icone" width={25} height = {25} src = "/img/header_search.svg" alt=""/>*/}
-                  <div className="Search"><img width={22} height={22} src = "/img/search.svg" alt="лупа"/><input placeholder ="Поиск пиццы..."></input></div>
-                  <div className="ShoppingBasket">
+                  <img className="react_header_logo.svg"  src = "/img/react_header_logo.svg" alt=""/>
+                  <div className="search"><img width={22} height={22} src = "/img/search.svg" alt="лупа"/><input placeholder ="Поиск пиццы..."></input></div>
+                  <div className="shoppingBasket">
                         <div>0 ₽</div>
-                        <div className="Delimetr"></div>
+                        <div className="delimetr"></div>
                         <div>🗑 0</div>
                   </div>
 
