3a14afc40d70bc9689d8dd80ccd63031aa17b19e
Egor Pikalov <pikaov.ea97@gmail.com>
2022-10-11 19:59:16 +0300

    asdas

--- src/Componets/Header/Header.jsx
+++ src/Componets/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header(){
       return(
       
             <div className="header">
-                  <Link to = "/"><img className="react_header_logo.svg"  src = "/img/react_header_logo.svg" alt=""/></Link>
+                  <Link to = "/"><img className="react_header_logo"  src = "/img/react_header_logo.svg" alt=""/></Link>
                   <div className="search"><img width={22} height={22} src = "/img/search.svg" alt="лупа"/><input placeholder ="Поиск пиццы..."></input></div>
                   <Link style={{ textDecoration: 'none' }} to = "/Basket">
                         <div className="shoppingBasket">
